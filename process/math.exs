c3d69b3f9bfc7040d5d27cd4bc6a3879f06586b1
Chensienyong <ashxcellent_kingzr@yahoo.com>
2019-05-31 23:34:52 +0700

    messages

--- process/math.exs
+++ process/math.exs
@@ -1,6 +1,18 @@
 defmodule Math do
   def add(a, b) do
-    a + b
-    |> IO.inspect()
+    receive do
+      senders_pid ->
+        send(senders_pid, a + b)
+    end
+  end
+
+  def double(n) do
+    spawn(Math, :add, [n,n])
+    |> send(self())
+
+    receive do
+      doubled ->
+        doubled
+    end
   end
 end
